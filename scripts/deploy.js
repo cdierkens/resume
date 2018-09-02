b45270c9e0f7a2141e0286c9f73836f10174dc08
Christopher Dierkens <cjdierkens@gmail.com>
2018-09-02 18:43:05 -0400

    Skip build on gh-pages branch

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,31 @@
 #!/usr/bin/env node
 
 const ghpages = require('gh-pages');
+const fs = require('fs');
 
 const options = {
+  dotfiles: true,
   message: 'Auto generated commit for gh-pages deploy.',
+  repo: 'git@github.com:cdierkens/resume.git',
   user: {
-      email: 'cjdierkens@gmail.com',
-      name: 'Christopher Dierkens'
+    email: 'cjdierkens@gmail.com',
+    name: 'Christopher Dierkens'
   }
 };
+  
+fs.mkdirSync('build/.circleci');
+fs.writeFileSync('build/.circleci/config.yml', `version: 2
+jobs:
+build:
+    machine: true
+    steps:
+    - run: echo "Skipping tests on gh-pages branch"
+`);
 
 function callback (err) {
   if (err) {
-      process.stderr.write(err.message + '\n');
-      return process.exit(1);
+    process.stderr.write(err.message + '\n');
+    return process.exit(1);
   }
   process.stdout.write('Published\n');
 }
